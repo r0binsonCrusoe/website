897e52e7aa4f1bf5c12e048fc4fea892c9878c30
Craig C <cwc.cal@gmail.com>
2024-10-21 21:46:47 -0400

    update gallery/forms.py

--- ccdjang/gallery/forms.py
+++ ccdjang/gallery/forms.py
@@ -4,4 +4,4 @@ from .models import Photo
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ['title', 'image', 'description']
+        fields = ['title', 'image', 'description','location']
