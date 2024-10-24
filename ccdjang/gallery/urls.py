e7b4037698d34cb30d69823eaa88ec279bb70a94
Craig C <cwc.cal@gmail.com>
2024-10-23 20:33:05 -0400

    add PhotoConfig to settings.py installed apps

--- ccdjang/gallery/urls.py
+++ ccdjang/gallery/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gallery, name='gallery'),
+    path('', views.index, name='index'),
+    path('gallery/', views.gallery, name='gallery'),
     path('upload/', views.upload_photo, name='upload_photo'),
 ]
